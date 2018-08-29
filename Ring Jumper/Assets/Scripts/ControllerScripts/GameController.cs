3294d153273f77290bf72f420d7396c2f0892cb5
Raymond Liang <rliang1994@gmail.com>
2018-08-28 21:42:05 -0700

    more stuff

---
+++ Ring Jumper/Assets/Scripts/ControllerScripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    private Player playerColor;
+    private SpriteRenderer playerSR;
+
+    private Color[] colors;
+
+	// Use this for initialization
+	void Start () {
+        colors = new Color[4];
+        // yellow
+        colors[0] = new Color(245 / 255f, 222 / 255f, 22 / 255f, 1f);
+        // pink
+        colors[1] = new Color(244 / 255f, 5 / 255f, 132 / 255f, 1f);
+        // cyan
+        colors[2] = new Color(48 / 255f, 228 / 255f, 242 / 255f, 1f);
+        // purple
+        colors[3] = new Color(143 / 255f, 16 / 255f, 253 / 255f, 1f);
+        playerColor = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        playerSR = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(playerColor.activeColor == "yellow")
+            playerSR.color = colors[0];
+        if (playerColor.activeColor == "pink")
+            playerSR.color = colors[1];
+        if (playerColor.activeColor == "cyan")
+            playerSR.color = colors[2];
+        if (playerColor.activeColor == "purple")
+            playerSR.color = colors[3];
+    }
+}
--- Ring Jumper/Assets/Scripts/Player Scripts/Player.cs
+++ Ring Jumper/Assets/Scripts/Player Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour {
     private string[] colorNames;
     public string activeColor;
 
+    private bool canJump = true;
+
     void Awake()
     {
         colorNames = new string[4];
@@ -30,9 +32,38 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButton(0) || Input.GetKeyDown(KeyCode.Space))
+        if (canJump)
         {
-            myBody.velocity = Vector2.up * jump;
+            if (Input.GetMouseButton(0) || Input.GetKeyDown(KeyCode.Space))
+            {
+                myBody.velocity = Vector2.up * jump;
+            }
         }
 	}
+    public void GameOver()
+    {
+        StartCoroutine(Death());
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "star")
+        {
+            Destroy(target.gameObject);
+        }
+        if(target.tag == "color")
+        {
+            int randomColor = Random.Range(0, colorNames.Length);
+            activeColor = colorNames[randomColor];
+            Destroy(target.gameObject);
+        }
+    }
+
+    IEnumerator Death()
+    {
+        transform.position = new Vector2(50, transform.position.y);
+        myBody.isKinematic = true;
+        canJump = false;
+        yield return new WaitForSeconds(0.3f);
+    }
 }

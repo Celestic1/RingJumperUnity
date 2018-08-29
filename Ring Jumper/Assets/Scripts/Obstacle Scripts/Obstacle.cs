ae840d7267e638555afd1006a26f680ba06da84c
Raymond Liang <rliang1994@gmail.com>
2018-08-28 19:16:21 -0700

    added ring and player

---
+++ Ring Jumper/Assets/Scripts/Obstacle Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    private Player player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+	
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(player.activeColor != this.tag)
+        {
+            print("Game Over");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Ring Jumper/Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float jump = 7f;
+    [SerializeField]
+    private Rigidbody2D myBody;
+
+    private string[] colorNames;
+    public string activeColor;
+
+    void Awake()
+    {
+        colorNames = new string[4];
+        colorNames[0] = "yellow";
+        colorNames[1] = "pink";
+        colorNames[2] = "cyan";
+        colorNames[3] = "purple";
+
+        activeColor = "";
+    }
+
+	// Use this for initialization
+	void Start () {
+        int randomColor = Random.Range(0, colorNames.Length);
+        activeColor = colorNames[randomColor];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            myBody.velocity = Vector2.up * jump;
+        }
+	}
+}

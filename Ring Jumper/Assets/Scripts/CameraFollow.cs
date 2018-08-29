e0902efb79ab89454104230a059fdff0df7d3013
Raymond Liang <rliang1994@gmail.com>
2018-08-28 20:08:25 -0700

    obstacle

---
+++ Ring Jumper/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 camera = transform.position;
+        camera.y = player.position.y;
+        transform.position = camera;
+	}
+}
